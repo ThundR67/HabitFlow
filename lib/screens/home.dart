57798d146d2013e666ffb54124b6314395c30877
Roshan Mehta <sonicroshan122@gmail.com>
2020-07-11 12:52:11 +0530

    Created basic layout with a bottom navigation bar

---
+++ lib/screens/home.dart
@@ -0,0 +1,75 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:flutter/material.dart';
+
+/// A page which has bottom navigation bar and shows all main pages.
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (int index) {
+            setState(() => _currentIndex = index);
+          },
+          children: <Widget>[
+            Container(color: Colors.blue),
+            Container(color: Colors.red),
+            Container(color: Colors.deepOrange),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        showElevation: true,
+        onItemSelected: (int index) => setState(() {
+          _currentIndex = index;
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.ease,
+          );
+        }),
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            icon: const Icon(Icons.refresh),
+            title: const Text('Cycles'),
+            activeColor: Colors.blue,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.today),
+            title: const Text('Today'),
+            activeColor: Colors.red,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.star),
+            title: const Text('Rewards'),
+            activeColor: Colors.deepOrange,
+          ),
+        ],
+      ),
+    );
+  }
+}
