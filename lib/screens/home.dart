f470ab27829846595c71e40a95a0dae1241176e8
Roshan Mehta <sonicroshan122@gmail.com>
2020-07-11 13:01:32 +0530

    Setted initial page to 1

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,13 +10,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _currentIndex = 0;
+  int _currentIndex = 1;
   PageController _pageController;
 
   @override
   void initState() {
     super.initState();
-    _pageController = PageController();
+    _pageController = PageController(initialPage: 1);
   }
 
   @override
