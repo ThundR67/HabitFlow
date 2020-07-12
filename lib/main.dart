de3efd8da8a61f7a0a1e992f3385fdb3c7a99984
Roshan Mehta <sonicroshan122@gmail.com>
2020-07-12 15:07:04 +0530

    Using neumorphic design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:provider/provider.dart';
 
 import 'package:habitflow/blocs/rewards_bloc.dart';
@@ -12,7 +13,13 @@ void main() {
         ChangeNotifierProvider<RewardsBloc>(create: (_) => RewardsBloc()),
         ChangeNotifierProvider<PointsBloc>(create: (_) => PointsBloc()),
       ],
-      child: const MaterialApp(
+      child: const NeumorphicApp(
+        themeMode: ThemeMode.dark,
+        darkTheme: NeumorphicThemeData(
+          baseColor: Color(0xFF3E3E3E),
+          lightSource: LightSource.topLeft,
+          depth: 6,
+        ),
         home: Home(),
       ),
     ),
