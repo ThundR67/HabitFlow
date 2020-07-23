91991747cd0c1ad90ff2483317699b0022fde568
Roshan Mehta <sonicroshan122@gmail.com>
2020-07-23 16:53:16 +0530

    Created cycles dao

--- lib/services/cycles/dao.dart
+++ lib/services/cycles/dao.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:habitflow/models/day.dart';
 import 'package:sembast/sembast.dart';
 import 'package:habitflow/models/cycle.dart';
 
@@ -15,14 +14,18 @@ class CyclesDAO {
 
   Future<Database> get _db async => await DB.instance.database;
 
-  Finder _finder(String id) => Finder(filter: Filter.equals(idKey, id));
-
   /// Adds a cycle into db.
   Future<void> add(Cycle cycle) async {
     await _store.add(await _db, cycle.toMap());
   }
 
-  /// Returns all cycles sorted by cycle points required.
+  /// Updates a cycle.
+  Future<void> update(Cycle cycle) async {
+    final Finder finder = Finder(filter: Filter.equals(idKey, cycle.id));
+    await _store.update(await _db, cycle.toMap(), finder: finder);
+  }
+
+  /// Returns all cycles sorted by cycle start dates.
   Future<List<Cycle>> all() async {
     final Finder finder =
         Finder(sortOrders: <SortOrder>[SortOrder(startKey, false)]);
@@ -39,32 +42,6 @@ class CyclesDAO {
       cycle.id = snapshot.key;
       return cycle;
     }).toList();
-  }
-
-  /// Returns a specific cycle of [date].
-  Future<Cycle> getFromDate(DateTime date) async {
-    final Finder finder =
-        Finder(filter: Filter.equals(startKey, Day.format(date)));
-    final RecordSnapshot<String, Map<String, dynamic>> snapshot =
-        await _store.findFirst(await _db, finder: finder);
-    if (snapshot == null) {
-      return null;
-    }
-    return Cycle.fromMap(snapshot.value);
-  }
-
-  /// Updates a cycle in db.
-  Future<void> update(Cycle cycle) async {
-    await _store.update(
-      await _db,
-      cycle.toMap(),
-      finder: _finder(cycle.id),
-    );
-  }
-
-  /// Deletes a cycle from db.
-  Future<void> delete(Cycle cycle) async {
-    await _store.delete(await _db, finder: _finder(cycle.id));
   }
 
   /// Clears db.
