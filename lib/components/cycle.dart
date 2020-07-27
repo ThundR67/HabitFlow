ae52809dde42f7a27dc8d255ad3ee6fceb0dc271
Roshan Mehta <sonicroshan122@gmail.com>
2020-07-27 14:42:53 +0530

    CycleCard redirects to CycleInfo

--- lib/components/cycle.dart
+++ lib/components/cycle.dart
@@ -3,6 +3,7 @@ import 'package:habitflow/components/neu_card.dart';
 import 'package:habitflow/models/cycle.dart';
 import 'package:habitflow/models/dates.dart';
 import 'package:habitflow/models/success_rate.dart';
+import 'package:habitflow/screens/cycle.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
 const List<String> _months = <String>[
@@ -33,42 +34,55 @@ class CycleCard extends StatelessWidget {
     final DateTime end = parseDate(_cycle.end);
     final double successRate = calculateSuccessRate(_cycle.days);
     return NeuCard(
-      child: Container(
-        padding: const EdgeInsets.all(8.0),
-        alignment: Alignment.center,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              _months[start.month - 1],
-              style: const TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () => Navigator.push<CycleInfo>(
+            context,
+            MaterialPageRoute<CycleInfo>(
+              builder: (BuildContext context) => CycleInfo(_cycle),
+            ),
+          ),
+          child: Ink(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    _months[start.month - 1],
+                    style: const TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '${start.day} - ${end.day}',
+                    style: const TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  CircularPercentIndicator(
+                    radius: 64.0,
+                    lineWidth: 4.0,
+                    progressColor: Colors.greenAccent[400],
+                    percent: successRate,
+                    backgroundColor: Colors.transparent,
+                    center: Text(
+                      (successRate * 100).toStringAsFixed(1) + '%',
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text(
-              '${start.day} - ${end.day}',
-              style: const TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            CircularPercentIndicator(
-              radius: 64.0,
-              lineWidth: 4.0,
-              progressColor: Colors.greenAccent[400],
-              percent: successRate,
-              backgroundColor: Colors.transparent,
-              center: Text(
-                (successRate * 100).toStringAsFixed(1) + '%',
-                style: const TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
--- lib/screens/cycle.dart
+++ lib/screens/cycle.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:habitflow/models/cycle.dart';
 
 /// Shows data about a cycle.
 class CycleInfo extends StatelessWidget {
   /// Constructs.
-  const CycleInfo({Key key}) : super(key: key);
+  const CycleInfo(this._cycle, {Key key}) : super(key: key);
+
+  final Cycle _cycle;
 
   @override
   Widget build(BuildContext context) {
