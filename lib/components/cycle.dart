89387835e451a8df0dacd2319bc60ee4ede791a7
Roshan Mehta <sonicroshan122@gmail.com>
2020-07-27 14:47:52 +0530

    Moved cycle status to seperate file

--- lib/components/cycle.dart
+++ lib/components/cycle.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:habitflow/components/cycle_status.dart';
 import 'package:habitflow/components/neu_card.dart';
 import 'package:habitflow/models/cycle.dart';
-import 'package:habitflow/models/dates.dart';
-import 'package:habitflow/models/success_rate.dart';
 import 'package:habitflow/screens/cycle.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
-
-const List<String> _months = <String>[
-  'JAN',
-  'FEB',
-  'MAR',
-  'APR',
-  'MAY',
-  'JUN',
-  'JUL',
-  'AUG',
-  'SEP',
-  'OCT',
-  'NOV',
-  'DEC',
-];
 
 /// A card to show cycle data.
 class CycleCard extends StatelessWidget {
@@ -30,9 +13,6 @@ class CycleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final DateTime start = parseDate(_cycle.start);
-    final DateTime end = parseDate(_cycle.end);
-    final double successRate = calculateSuccessRate(_cycle.days);
     return NeuCard(
       child: Material(
         color: Colors.transparent,
@@ -47,40 +27,7 @@ class CycleCard extends StatelessWidget {
             child: Container(
               padding: const EdgeInsets.all(8.0),
               alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    _months[start.month - 1],
-                    style: const TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    '${start.day} - ${end.day}',
-                    style: const TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  CircularPercentIndicator(
-                    radius: 64.0,
-                    lineWidth: 4.0,
-                    progressColor: Colors.greenAccent[400],
-                    percent: successRate,
-                    backgroundColor: Colors.transparent,
-                    center: Text(
-                      (successRate * 100).toStringAsFixed(1) + '%',
-                      style: const TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              child: CycleStatus(_cycle),
             ),
           ),
         ),
