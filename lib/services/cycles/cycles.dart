29c4ca1319d859316e7df5005d7abc4267c9e9cd
Roshan Mehta <sonicroshan122@gmail.com>
2020-08-04 15:36:32 +0530

    Cleaned cycles service

--- lib/services/cycles/cycles.dart
+++ lib/services/cycles/cycles.dart
@@ -3,41 +3,40 @@ import 'dart:async';
 import 'package:sembast/sembast.dart';
 
 import 'package:habitflow/models/cycle.dart';
-import 'db.dart';
+import 'package:habitflow/services/database/database.dart';
 
-const String _storeName = 'cycles';
+/// Name of db.
+const String _dbName = 'cycles';
 
 /// A DAO to manage user's cycle points.
 class CyclesDAO {
+  /// Store of data.
   final StoreRef<String, Map<String, dynamic>> _store =
-      stringMapStoreFactory.store(_storeName);
+      stringMapStoreFactory.store(_dbName);
 
-  Future<Database> get _db async => await DB.instance.database;
+  /// Connection to db.
+  Future<Database> get _db async => await DB.instance.database(_dbName);
 
   /// Adds a cycle into db.
   Future<void> add(Cycle cycle) async {
     await _store.add(await _db, cycle.toMap());
   }
 
-  /// Returns all cycles sorted by cycle start dates and with key id.
+  /// Returns all cycles reversly sorted by cycle start dates.
   Future<Map<String, Cycle>> all() async {
-    final Finder finder =
-        Finder(sortOrders: <SortOrder>[SortOrder(startKey, false)]);
-
     final List<RecordSnapshot<String, Map<String, dynamic>>> snapshots =
         await _store.find(
       await _db,
-      finder: finder,
+      finder: Finder(sortOrders: <SortOrder>[SortOrder(startKey, false)]),
     );
 
-    final List<Cycle> cycles =
-        snapshots.map((RecordSnapshot<String, Map<String, dynamic>> snapshot) {
-      final Cycle cycle = Cycle.fromMap(snapshot.value);
-      cycle.id = snapshot.key;
-      return cycle;
-    }).toList();
+    final List<Cycle> cycles = snapshots.map(
+      (RecordSnapshot<String, Map<String, dynamic>> snapshot) {
+        return Cycle.fromMap(snapshot.value);
+      },
+    ).toList();
 
-    return <String, Cycle>{for (Cycle e in cycles) e.id: e};
+    return <String, Cycle>{for (Cycle cycle in cycles) cycle.id: cycle};
   }
 
   /// Clears db.
