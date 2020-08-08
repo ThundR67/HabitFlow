cb90e5bd97ab4f50ec887262223f5ac0f3d29309
Roshan Mehta <sonicroshan122@gmail.com>
2020-08-08 18:37:25 +0530

    Added splash screen

--- lib/helpers/intro.dart
+++ lib/helpers/intro.dart
@@ -3,9 +3,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 /// Main intro.
 const String mainIntro = 'isIntroShown';
 
-/// Intro for reward points in reward screen.
-const String rewardPointsIntro = 'rewardPointsIntro';
-
 /// Returns if intro screen was shown.
 Future<bool> isIntroShown(String key) async {
   final SharedPreferences pref = await SharedPreferences.getInstance();
