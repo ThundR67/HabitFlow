97fc422b7821586d23e00ff179f50102f1e2f1ce
Roshan Mehta <sonicroshan122@gmail.com>
2020-08-11 14:24:58 +0530

    Updated colors

--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -11,7 +11,7 @@ NeumorphicStyle neuStyle(
   final bool isLight = Theme.of(context).brightness == Brightness.light;
   final Color color = Theme.of(context).scaffoldBackgroundColor;
   final Color dark = TinyColor(color).darken(isLight ? 20 : 3).color;
-  final Color light = TinyColor(color).lighten(isLight ? 24 : 8).color;
+  final Color light = TinyColor(color).lighten(isLight ? 24 : 6).color;
 
   return NeumorphicStyle(
     boxShape: NeumorphicBoxShape.roundRect(
