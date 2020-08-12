25be5cb8737f4dffa22e3137662dd11ac0e4c563
Roshan Mehta <sonicroshan122@gmail.com>
2020-08-12 12:05:40 +0530

    Migrated intro service

--- lib/services/intro/intro.dart
+++ lib/services/intro/intro.dart
@@ -1,7 +1,7 @@
 /// A service to manage all introductions.
 import 'dart:async';
 
-import 'package:sembast/sembast.dart';
+import 'package:hive/hive.dart';
 
 import 'package:habitflow/services/database/database.dart';
 
@@ -10,22 +10,19 @@ const String _dbName = 'introduction';
 
 /// A DAO to manage all introductions.
 class IntroDAO {
-  /// Store of data.
-  final StoreRef<String, bool> _store = StoreRef<String, bool>.main();
-
   /// Connection to db.
-  Future<Database> get _db async => DB.instance.database(_dbName);
+  Future<Box> get _db => openDatabase(_dbName);
 
   /// Set an intro as true.
   Future<void> introShown(String name) async {
-    await _store.record(name).put(await _db, true);
+    await (await _db).put(name, true);
   }
 
   /// Returns if an intro is true.
   Future<bool> isShown(String name) async {
-    return (await _store.record(name).get(await _db)) ?? false;
+    return (await _db).get(name, defaultValue: false) as bool;
   }
 
   /// Clears db.
-  Future<void> clear() async => _store.drop(await _db);
+  Future<void> clear() async => (await _db).clear();
 }
