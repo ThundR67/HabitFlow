4a73faa16f2c7a0ccdf0a6beb106248a178c4bd1
Roshan Mehta <sonicroshan122@gmail.com>
2020-08-14 11:13:03 +0530

    Updated translations

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -3,14 +3,14 @@
 //
 
 // ignore: unused_import
-
 import 'dart:ui';
 
 import 'package:audioplayers/audioplayers_web.dart';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
