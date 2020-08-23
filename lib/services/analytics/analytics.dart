572a19dab59634ce9db7d9d67f37c07b5f5c5a08
Roshan Mehta <sonicroshan122@gmail.com>
2020-08-23 17:29:36 +0530

    Cleaned analytics service

--- lib/services/analytics/analytics.dart
+++ lib/services/analytics/analytics.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
-import 'package:flutter/foundation.dart';
 import 'package:habitflow/models/habit.dart';
 import 'package:habitflow/models/reward.dart';
 
@@ -15,24 +14,21 @@ class Analytics {
 
   /// Initializes firebase analytics.
   void init() {
-    if (!kIsWeb) _analytics = FirebaseAnalytics();
+    _analytics = FirebaseAnalytics();
   }
 
   /// Returns an oberser for firebase analytics.
   FirebaseAnalyticsObserver observer() {
-    if (!kIsWeb) return null;
     return FirebaseAnalyticsObserver(analytics: _analytics);
   }
 
   /// Logs an simple event with [name].
   void logSimple(String name) {
-    if (kIsWeb) return;
     _analytics.logEvent(name: name);
   }
 
   /// Logs a reward with event [name].
   void logHabit(String name, Habit habit) {
-    if (kIsWeb) return;
     _analytics.logEvent(
       name: name,
       parameters: {
@@ -44,7 +40,6 @@ class Analytics {
 
   /// Logs a reward with event [name].
   void logReward(String name, Reward reward) {
-    if (kIsWeb) return;
     _analytics.logEvent(
       name: name,
       parameters: {
