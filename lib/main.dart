bb545e33e2329624eaf0d4c659da26e88e473445
Roshan Mehta <sonicroshan122@gmail.com>
2020-08-29 22:23:51 +0530

    Add transparent status bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:global_configuration/global_configuration.dart';
 import 'package:habitflow/blocs/ad_bloc.dart';
@@ -46,6 +47,11 @@ Future<void> main() async {
   Analytics().init();
   await _hiveInit();
   await GlobalConfiguration().loadFromAsset("dev.json");
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
 
   runApp(
     MultiProvider(
