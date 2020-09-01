218929ab81407838db163d5b6fcac033efd8b577
Roshan Mehta <sonicroshan122@gmail.com>
2020-09-01 21:52:00 +0530

    Custom weekday selector

--- lib/components/weekdays_picker.dart
+++ lib/components/weekdays_picker.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:habitflow/resources/strings.dart';
 
-import 'package:weekday_selector/weekday_selector.dart';
-
-/// TODO change package.
-
-/// A widget to allow user to pick weekdays.
+/// A picker to allow user to pick weekdays.
 class WeekdaysPicker extends StatefulWidget {
   /// Constructs.
   const WeekdaysPicker({@required this.onChange, @required this.color});
 
   /// Function to run when weekdays change.
-  final Function onChange;
+  final Function(List<int>) onChange;
 
   /// Color of the widget.
   final Color color;
@@ -20,38 +17,79 @@ class WeekdaysPicker extends StatefulWidget {
 }
 
 class _WeekdaysPickerState extends State<WeekdaysPicker> {
-  final List<bool> values = List<bool>.filled(7, false);
+  final List<int> _daysSelected = [];
 
-  /// Updates [_values].
-  void _updateValues(int day) {
-    setState(() {
-      final int index = day % 7;
-      values[index] = !values[index];
-    });
-  }
-
-  /// Creates list of active days and calls [widget.onChange].
-  void _onChange() {
-    final List<int> activeDays = <int>[];
-    for (int i = 0; i < values.length; i++) {
-      if (values[i] && i == 0) {
-        activeDays.add(7);
-      } else if (values[i]) {
-        activeDays.add(i);
-      }
+  /// Adds [day] to [_daysSelected] if not exists, else removes it.
+  void _onSelect(int day) {
+    if (_daysSelected.contains(day)) {
+      _daysSelected.remove(day);
+    } else {
+      _daysSelected.add(day);
     }
-    widget.onChange(activeDays);
+    widget.onChange(_daysSelected);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return WeekdaySelector(
-      selectedFillColor: widget.color,
-      onChanged: (int day) {
-        _updateValues(day);
-        _onChange();
-      },
-      values: values,
+    return Container(
+      height: 40,
+      child: ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: DateTime.daysPerWeek,
+        itemBuilder: (context, index) => _Day(
+          index: index + 1,
+          isSelected: _daysSelected.contains(index + 1),
+          onSelect: _onSelect,
+          activeColor: widget.color,
+        ),
+      ),
+    );
+  }
+}
+
+/// A circular card.
+class _Day extends StatelessWidget {
+  const _Day({
+    @required this.index,
+    @required this.isSelected,
+    @required this.onSelect,
+    @required this.activeColor,
+  });
+
+  /// Index of the day. (Starts from 1, which is of monday).
+  final int index;
+
+  /// If the day selected.
+  final bool isSelected;
+
+  /// On card is selected.
+  final Function(int) onSelect;
+
+  /// Color when card is selected.
+  final Color activeColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      child: InkWell(
+        onTap: () => onSelect(index),
+        child: Card(
+          elevation: isSelected ? 8 : 2,
+          color: isSelected ? activeColor : Theme.of(context).cardColor,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(100),
+            ),
+          ),
+          child: Center(
+            child: Text(weekdays[index - 1][0]),
+          ),
+        ),
+      ),
     );
   }
 }
